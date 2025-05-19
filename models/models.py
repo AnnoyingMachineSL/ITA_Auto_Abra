582945b259377f764ce5a5802ab00604d975b614
AnnoyingMachineSL <runn1k195@gmail.com>
2025-05-19 20:48:46 +0300

    new project

---
+++ models/models.py
@@ -0,0 +1,46 @@
+from typing import Optional, Union
+import json
+from pydantic import BaseModel, Field
+
+#Components
+class NegativeLoginModelComponents(BaseModel):
+    loc: Optional[list]
+    msg: Optional[str]
+    type: Optional[str]
+
+class NegativeRegistrationModelSecondComponents(BaseModel):
+    loc: Optional[list]
+    msg: Optional[str]
+    type: Optional[str]
+    ctx: Optional[dict]
+
+
+
+# Request models
+class LoginModel(BaseModel):
+    email: Optional[str]
+    password: Optional[str]
+
+
+
+# Response models
+
+class LoginResponseModel(BaseModel):
+    ok: Optional[bool] = True
+    result: Optional[bool] = True
+
+class RegistrationResponseModel(BaseModel):
+    ok: Optional[bool] = True
+    result: Optional[bool] = None
+    detail: Optional[dict] = {}
+
+
+
+
+#Negative response models
+class NegativeLoginResponseModel(BaseModel):
+    detail: Optional[list] = [NegativeLoginModelComponents]
+
+
+class NegativeRegistrationResponseModel(BaseModel):
+    detail: Optional[list] = [NegativeLoginModelComponents, NegativeRegistrationModelSecondComponents]
---
+++ utils/config.py
@@ -0,0 +1,3 @@
+class APILogin:
+    LOGIN = 'runn1k195@gmail.com'
+    PASSWORD = 'QWE123!qwe123'
